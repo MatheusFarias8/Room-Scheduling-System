75cb8dab439ee14fb41660e9114a78b8428add67
Matheus Farias <matheusfarias3456@hotmail.com>
2026-02-14 09:43:18 -0300

    first commit

---
+++ backend-api/src/main/java/com/Unichristus/Room_Scheduling_System/RoomSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Unichristus.Room_Scheduling_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomSchedulingSystemApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/Unichristus/Room_Scheduling_System/RoomSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Unichristus.Room_Scheduling_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomSchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
