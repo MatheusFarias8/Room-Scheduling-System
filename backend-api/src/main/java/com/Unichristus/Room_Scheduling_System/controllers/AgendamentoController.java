a0de804ac62f2f281c422ad98238ec589d6de7f0
Matheus Farias <matheusfarias3456@hotmail.com>
2026-02-15 09:40:31 -0300

    criando o controller

---
+++ backend-api/src/main/java/com/Unichristus/Room_Scheduling_System/controllers/AgendamentoController.java
@@ -0,0 +1,53 @@
+package com.Unichristus.Room_Scheduling_System.controllers;
+
+import com.Unichristus.Room_Scheduling_System.domain.dtos.agendamento.AgendamentoRequestDTO;
+import com.Unichristus.Room_Scheduling_System.domain.dtos.agendamento.AgendamentoResponseDTO;
+import com.Unichristus.Room_Scheduling_System.services.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/agendamentos")
+@RequiredArgsConstructor
+public class AgendamentoController {
+
+    private final AgendamentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<AgendamentoResponseDTO>> listarTodos() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AgendamentoResponseDTO> buscarPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AgendamentoResponseDTO> criar(
+            @RequestBody AgendamentoRequestDTO dto
+    ) {
+        AgendamentoResponseDTO criado = service.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(criado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AgendamentoResponseDTO> atualizar(
+            @PathVariable UUID id,
+            @RequestBody AgendamentoRequestDTO dto
+    ) {
+        return ResponseEntity.ok(service.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable UUID id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
